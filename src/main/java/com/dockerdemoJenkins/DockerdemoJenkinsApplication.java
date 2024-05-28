01b9dc427dcec8f265bfae4ad96020832e8c56ac
Jambunatha <jambunatha.koni@gmail.com>
2024-05-28 21:36:44 +0530

    intial input

---
+++ src/main/java/com/dockerdemoJenkins/DockerdemoJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.dockerdemoJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerdemoJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerdemoJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dockerdemoJenkins/DockerdemoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerdemoJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerdemoJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
